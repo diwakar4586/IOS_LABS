ae45401884c1246de09c37453aa22e27f5cb86ad
Diwakar <diwakar0470.be21@chitkara.edu.in>
2024-04-28 01:01:14 +0530

    Update Contents.swift

--- 1 - Getting Started/2 - Constants, Variables, and Data Types/Lab - Constants and Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/2 - Constants, Variables, and Data Types/Lab - Constants and Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
@@ -11,7 +11,7 @@ friends = friends - 7
 
 
 //:  Does the above code compile? Why not? Print your explanation to the console using the `print` function. Go back and delete your line of code that updates the `friends` constant to a lower number so that the playground will compile properly.
-print("well we can't reassign values to constants, we can change let to var for reassigning")
+print("well  can't reassign values to constants, we can change let to var for reassigning")
 
 print(friends)
 /*:
